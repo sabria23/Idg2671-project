0870ae09151fb44be9c6451e0936ee32dfdda5d1
modesta <trakselytemodesta@gmail.com>
2025-03-12 21:58:32 +0100

    having issues conencting to mongodb..

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import colors from "colors";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 import router from './Routes/dashRoutes.js';
+import {connectToDatabases} from "./Config/db.js";
+
+
 dotenv.config();
 // Get the current directory of the ES module (server.js)
 const __filename = fileURLToPath(import.meta.url);
@@ -11,7 +15,7 @@ dotenv.config({ path: resolve(__dirname, '../.env') });
 const port = process.env.PORT || 3000;
 const app = express();
 
-
+connectToDatabases();
 
 app.use(express.json());
 // in order to see request from the body we need this line (if using postman for example)
