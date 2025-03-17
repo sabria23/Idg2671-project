1656270fbcb9ccda5ba6ef8a5cc18f01a9a51856
sabrina <sabrina130102@gmail.com>
2025-03-17 10:12:12 +0100

    create routes in app.js

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import LoginSignup from "./Components/LoginSignup/LoginSignup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage/Homepage.jsx";
 import Dashboard from "./Pages/Dashboard/dashboard.jsx";
 
 function App() {
   return (
-    <div>
-      <LoginSignup/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="homepage" element={<Homepage/>}></Route>
+        <Route path="homepage" element={<Dashboard/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
