0b78f147778a3616d923651446c06b5f24e1ebb5
modesta <trakselytemodesta@gmail.com>
2025-03-26 11:24:26 +0100

    updated dashborad routes and controllers

--- Backend/Controllers/dashController.js
+++ Backend/Controllers/dashController.js
@@ -2,16 +2,15 @@
 import { v4 as uuidv4 } from "uuid";
 //import Study from '../Models/studyModel.js';
 
-
 // @desc Get all studies
-// @route GET /api/dash-studies
+// @route GET /api/studies
 // @access Private (after auth is added)
 const getAllStudies = async (req, res) => {
    
 };
 
 // @desc Delete a study
-// @route DELETE /api/dash-studies/:studyId
+// @route DELETE /api/studies/:studyId
 // @access Private (after auth is added)
 // IMPORTANT THE REQ.PARAMS.STUDYID i think i need to use this oen for the postman
 const deleteStudy = async (req, res) => {
@@ -19,44 +18,47 @@ const deleteStudy = async (req, res) => {
 };
 
 // @desc Get responses for a study (for export page)
-// @route GET /api/dash-studies/:studyId/responses
+// @route GET /api/studies/:studyId/responses
 // @access Private (after auth is added)
+// Consider adding pagination if you expect a large number of responses
 const getResponses = (req, res) => {
     res.status(200).json({ message: `Get responses for study ${req.params.studyId}` });
 };
 
-// @desc Export study data as JSON
-// @route GET /api/dash-studies/:studyId/responses/export-json
-// @access Private (after auth is added)
-const exportJson = (req, res) => {
-    res.status(200).json({ message: `Export study ${req.params.studyId} as JSON` });
-};
 
-// @desc Generate a unique shareable link for a study
-// @route POST /api/dash-studies/:studyId/generate-link
+// @desc update status of the study (publish/unpubloshed)
+// @route PATCH /api/studies/:studyId/public
+// @access Private (after auth is added)
+const updateStudyStatus = (req, res) => {
+    res.status(200).json({ message: ` You have published study ${req.params.studyId}, get your URl Link` });
+}
+
+// @desc generate a URL link to publish that quiz
+// @route POST /api/studies/:studyId/generate-link
 // @access Private (after auth is added)
 const generateLink = (req, res) => {
-     res.status(200).json({ message: `Generate link for study ${req.params.studyId}` });
+    res.status(200).json({ message: `Generate link for study ${req.params.studyId}` });
 };
+
+
 
 // @desc Add participants via email
-// @route POST /api/dash-studies/:studyId/participants
+// @route POST /api/studies/:studyId/participants
 // @access Private (after auth is added)
-const addParticipants = (req, res) => {
+const emailInvitaitons = (req, res) => {
     res.status(200).json({ message: `Add participants to study ${req.params.studyId}` });
 };
 
-const updateStudyStatus = (req, res) => {
-    res.status(200).json({ message: `Add participants to study ${req.params.studyId}` });
-};
+
+
+
 
 export const dashController = {
     getAllStudies,
     deleteStudy,
     getResponses,
-    exportJson,
     generateLink,
-    addParticipants,
+    emailInvitaitons,
     updateStudyStatus
 };
 
--- Backend/Routes/dashRoutes.js
+++ Backend/Routes/dashRoutes.js
@@ -2,26 +2,13 @@ import express from "express";
 import {dashController} from "../Controllers/dashController.js";
 const dashRouter = express.Router();
 
-// Get all studies for the dashboard
-// /api/studies
+
 dashRouter.get('/', dashController.getAllStudies);
-
-// Delete a study from the dashboard
-// api/studies/sutudyId
 dashRouter.delete('/:studyId', dashController.deleteStudy);
-
-// Get responses for a study (for export page)
-// Consider adding pagination if you expect a large number of responses
 dashRouter.get('/:studyId/sessions/responses', dashController.getResponses);
-
-// update status of the study (publish/unpubloshed)
 dashRouter.patch('/:studyId/public', dashController.updateStudyStatus);
-
-//generate a URL link to publish that quiz
 dashRouter.post('/:studyId/public-url', dashController.generateLink);
-
-// Add participants via email
-dashRouter.post('/:studyId/invitations', dashController.addParticipants);
+dashRouter.post('/:studyId/invitations', dashController.emailInvitaitons);
 
 export default dashRouter;
 
