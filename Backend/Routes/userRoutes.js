87380c986b0b7e5eb78434ce4aba2a5ec7cc36cc
sabrina <sabrina130102@gmail.com>
2025-03-26 18:36:56 +0100

    created endpoints in userController.js: createUser, login, logout and getUser. Added authMiddleware.js, and jwt_secret in the .env

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,13 +1,15 @@
-import express from "express";
+import express from "express"; 
 import {userController} from "../Controllers/userController.js";
+
+
 const userRouter = express.Router();
 
-userRouter.post("/register", userController.createUser) 
-userRouter.post("/login", userController.authenticateLogin)
+userRouter.post("/register", userController.createUser);
+userRouter.post("/login", userController.authenticateLogin);
 userRouter.post("/logout", userController.authenticateLogout);
 userRouter.get("/user", userController.getUser);
-userRouter.post("/forgot-password")
-userRouter.post("/reset-apssword")
+// userRouter.post("/forgot-password")
+// userRouter.post("/reset-apssword")
 
 
 export default userRouter;
