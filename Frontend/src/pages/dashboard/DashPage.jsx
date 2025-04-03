64de21d3f6d3cd17e6fa4e1f13a340d52658bf65
modesta <trakselytemodesta@gmail.com>
2025-04-03 19:31:29 +0200

    setting up skeleton html layout for dashboard

--- Frontend/src/pages/dashboard/DashPage.jsx
+++ Frontend/src/pages/dashboard/DashPage.jsx
@@ -2,7 +2,28 @@ import styles from "../../styles/Dash.module.css";
 
 const DashboardPage = () => {
   return (
-    <div>DashPage</div>
+    <div className={styles.dashboardContainer}>
+        <nav className={styles.navbar}>
+            <div className={styles.logo}Study Platform></div>
+            <div className={styles.navLinks}>
+                <button>My studies</button>
+                <button>Account</button>
+                <button>Logout</button>
+            </div>
+        </nav>
+
+        <main className={styles.dashboradContent}>
+            <div className={styles.dashboradHeader}>
+                <h1>My Studies</h1>
+                <Link to="/create-study" className={styles.createStudyBtn}> Create New Study </Link>
+            </div>
+
+            <div className={styles.emptyStudyState}>
+                <h2>No studies created yet </h2>
+                <p>Create your first study to get started!</p>
+            </div>
+        </main>
+    </div>
   )
 }
 
