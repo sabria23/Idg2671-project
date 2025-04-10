48ef192413d27da1f84ac251853a2d6c5241eb76
Memeius <marsbj@stud.ntnu.no>
2025-04-10 02:16:02 +0200

    added display of info and demographics

--- Frontend/src/pages/survey/SurveyPage.jsx
+++ Frontend/src/pages/survey/SurveyPage.jsx
@@ -4,12 +4,14 @@ import UAParser from 'ua-parser-js';
 import axios from 'axios';
 import '../styles/displaySurvey.css';
 
+// IMPORTANT! Code should be splitted to components and have buttons for next and back!
+
 // make props for the page if it should be the real thing or a preview
 // a mode='preview' and a mode='live', preview will be accessible from the createStudy page and live from the link
 
 
 const {studyId} = useParams();
-const [step, setStep] = useState(0) // For managing the steps of the study 0 = intro, 1 = Demographics, 3
+const [step, setStep] = useState(0) // For managing the steps of the study 0 = intro, 1 = Demographics, 3 questions, 4 = submission and thank you
 const [studyInfo, setStudyInfo] = useState(null); // For title and description of the study
 
 // UseState to manage the demographics info and the session status to separate preview and live
@@ -19,6 +21,7 @@ const [sessionId, setSessionId] = useState(null);
 const [currentQuestion, setCurrentQuestion] = useState(null); 
 const [totalQuestions, setTotalQuestions] = useState(0); 
 
+// STEP 0 - INTRODUCTION
 
 // Fetching the study title and description from backend
 useEffect(() => {
@@ -42,6 +45,8 @@ useEffect(() => {
 
 const handleStart = () => setStep(1); // Go from the intro to demographics
 
+// STEP 1 - DEMOGRAPHICS
+
 // Handles the demographics and sends it to the backend and then receives the sessionId and proceeds to questions
 const handleDemographics = async () => {
     // UAParser to get the device information
@@ -60,14 +65,75 @@ const handleDemographics = async () => {
     setStep(2); // Go to the first question
 };
 
+// STEP 2 - QUESTIONS
+
+const handleAnswer = async (submitAnswer, skipped = false) => {
+    const questionId = currentQuestion._id;
+    try {
+        await axios.post(`/api/survey/${studyId}/sessions/${sessionId}/${questionId}`, {
+            participantAnswer: skipped ? null : submitAnswer,
+            skipped,
+            answerType: currentQuestion.questionType
+        });
+        
+    } catch (err) {
+        if (err.response?.status === 409) {
+            await axios.put(`/api/survey/${studyId}/sessions/${sessionId}/${questionId}`, {
+                participantAnswer: submitAnswer,
+                skipped,
+                answerType: currentQuestion.questionType
+            });
+        }
+    }
+    setStep(prev => prev + 1)
+}
 
 
 
+// STEP 3 - SUBMISSION AND THANK YOU
 
+if (step === 0) {
+    return (
+        <div>
+            <h1>{studyInfo.title}</h1>
+            <p>{studyInfo.description}</p>
+            <button onClick={handleStart}>Start</button>
+        </div>
+    )
+}
 
-return (
-    <>
+// !IMPORTANT! prefer not to say should maybe be otheroption instead
+if (step === 1) {
+    return (
+        <div>
+            <h1>Demographics</h1>
 
-    </>
-);
+                <label>Age:</label>
+                <select value={demographics.age} onChange={(e) = setDemographics(prev => ({...prev, age: e.target.value}))}>
+                    <option value="">Select</option>
+                    <option value="under 18">under 18</option>
+                    <option value="18-25">18-25</option>
+                    <option value="25-35">25-35</option>
+                    <option value="35-45">35-45</option>
+                    <option value="45-55">45-55</option>
+                    <option value="55-65">55-65</option>
+                    <option value="65+">65+</option>
+                </select>
+
+                <label>Gender:</label>
+                <select value={demographics.gender} onChange={(e) = setDemographics(prev => ({...prev, gender: e.target.value}))}>
+                    <option value="">Select</option>
+                    <option value="female">female</option>
+                    <option value="male">male</option>
+                    <option value="prefer_not_to_say">prefer not to say</option>
+                </select>
+
+                <button 
+                onClick={handleDemographics}
+                disabled={!demographics.age || !demographics.gender}
+                >Next</button>            
+        </div>
+    )
+}
+
 export default surveyPage;
